45c4d67a1dc4b8f044138c52fed35d7458c163c5
unknown <billbrown20000@gmail.com>
2020-03-16 16:54:50 -0400

    I fixed it

--- src/main/java/NameScanner.java
+++ src/main/java/NameScanner.java
@@ -4,13 +4,19 @@ public class NameScanner {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is your name");
+        System.out.println("What is your name?");
         String name = sc.nextLine();
 
-        if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("Bob")) {
-            System.out.println("Welcome" + name);
-        } else {
-            System.out.println("Wrong input try again");
-        }
+        while (!(name == ""))
+            if (name.equalsIgnoreCase("Alice") || name.equalsIgnoreCase("bob")) {
+                System.out.println("Welcome " + name);
+                return;
+            } else {
+                System.out.println("Wrong input try again");
+                System.out.println("What is your name?");
+                name = sc.nextLine();
+
+
+            }
     }
 }
